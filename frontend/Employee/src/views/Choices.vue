b6a6c8cffae553c1420ea06a76e8ba9c1adfe29c
Drew Harbaugh <dharbaugh2290@gmail.com>
2020-12-08 17:00:44 -0500

    Choices banner

--- frontend/Employee/src/views/Choices.vue
+++ frontend/Employee/src/views/Choices.vue
@@ -1,5 +1,21 @@
 <template>
+
   <div>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="danger" id="navBar">
+        <div class="navBarLogo" id="navbarLogo">
+          <b-img
+            src="./anotherOneBitesTheCrustLogo.png"
+            fluid
+            alt="Pizza image"
+          ></b-img>
+        </div>
+
+        <div id="title">
+          <h1 class="h3 mb-3 font-weight-normal">Menu Choices</h1>
+        </div>
+      </b-navbar>
+    </div>
     <choices-list v-bind:choices="$store.state.choices"></choices-list>
     <new-choice></new-choice>
   </div>
@@ -26,4 +42,25 @@ export default {
 </script>
 
 <style>
+#navbar {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas: "navbarLogo title .";
+}
+
+#navbarLogo {
+  grid-area: navbarLogo;
+}
+
+#title {
+  grid-area: title;
+  color: white;
+  display: flex;
+  justify-content: center;
+  padding-left: 23%;
+}
+
+.navBarLogo{
+  width: 20vw;
+}
 </style>
