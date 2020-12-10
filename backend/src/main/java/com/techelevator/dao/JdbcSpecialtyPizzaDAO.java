cdb180c0499cd35a899546df5493b10d68fa3c96
Jordan Jackson <jacks3jk@outlook.com>
2020-12-10 12:04:10 -0500

    specials GET now returns ingredients

--- backend/src/main/java/com/techelevator/dao/JdbcSpecialtyPizzaDAO.java
+++ backend/src/main/java/com/techelevator/dao/JdbcSpecialtyPizzaDAO.java
@@ -29,9 +29,8 @@ public class JdbcSpecialtyPizzaDAO implements SpecialtyPizzaDAO {
     @Override
     public List<SpecialtyPizza> getAll() {
         List<SpecialtyPizza> result = new ArrayList<>();
-        List<Choice> regularToppings = new ArrayList<>();
         List<Choice> premiumToppings = new ArrayList<>();
-        Choice sauce = new Choice();
+        List<Choice> regularToppings = new ArrayList<>();
 
 
         String sql = "SELECT specialty_id, name, description, price, is_available FROM specialty_pizza;";
@@ -45,64 +44,42 @@ public class JdbcSpecialtyPizzaDAO implements SpecialtyPizzaDAO {
             specialtyPizza.setDescription(rowSet.getString("description"));
 
 
-        String sql1 = "SELECT c.choice_id, c.category_id, c.name, c.is_available, s.name  " +
+        String sql1 = "SELECT c.choice_id, c.category_id, c.name, c.is_available, s.specialty_id, s.choice_id  " +
                 "FROM choices AS c " +
                 "JOIN choices_specialty_pizza s ON c.choice_id = s.choice_id " +
                 "WHERE s.specialty_id = ?;";
-//        SqlRowSet choiceRowSet = jdbcTemplate.queryForRowSet(sql1, specia)
+        SqlRowSet choiceRowSet = jdbcTemplate.queryForRowSet(sql1, specialtyPizza.getSpecialtyId());
+        while (choiceRowSet.next()) {
+            if (choiceRowSet.getInt("category_id") == 2) {
+
+                specialtyPizza.setCrust(mapRowToChoice(choiceRowSet));
+            }
+            if (choiceRowSet.getInt("category_id") == 3) {
+                specialtyPizza.setSauce(mapRowToChoice(choiceRowSet));
+            }
+            if (choiceRowSet.getInt("category_id") == 4) {
+                regularToppings.add(mapRowToChoice(choiceRowSet));
+            }
+            if (choiceRowSet.getInt("category_id") == 5) {
+                premiumToppings.add(mapRowToChoice(choiceRowSet));
+            }
         }
-
-
+        specialtyPizza.setRegularToppings(regularToppings);
+        specialtyPizza.setPremiumToppings(premiumToppings);
+        result.add(specialtyPizza);
+        }
         return result;
     }
 
+    private Choice mapRowToChoice(SqlRowSet rowSet) {
+        Choice choice = new Choice();
+        choice.setName(rowSet.getString("name"));
+        choice.setChoiceId(rowSet.getInt("choice_id"));
+        choice.setCategoryId(rowSet.getInt("category_id"));
+        choice.setAvailable(rowSet.getBoolean("is_available"));
+        return choice;
+    }
 
-
-//            String sqlSauce =
-//                    "SELECT s.choice_id, c.name " +
-//                    "FROM choices_specialty_pizza AS s " +
-//                    "JOIN choices AS c ON s.choice_id = c.choice_id " +
-//                    "WHERE s.specialty_id = ? AND c.category_id = 3;";
-//            SqlRowSet sauceRowSet = jdbcTemplate.queryForRowSet(sqlSauce, specialtyPizza.getSpecialtyId());
-//            if (sauceRowSet.next()) {
-//                int id = sauceRowSet.getInt("choice_id");
-//                sauce = jdbcChoiceDAO.getChoiceById(id);
-//
-//                specialtyPizza.setSauce(sauce);
-//            }
-//
-//            String sqlRegular =   "SELECT s.choice_id, c.name " +
-//                            "FROM choices_specialty_pizza AS s " +
-//                            "JOIN choices AS c ON s.choice_id = c.choice_id " +
-//                            "WHERE s.specialty_id = ? AND c.category_id = 4";
-//            SqlRowSet rowSet1 = jdbcTemplate.queryForRowSet(sqlRegular, specialtyPizza.getSpecialtyId());
-//            while (rowSet1.next()) {
-//                Choice regularChoice = new Choice();
-//                regularChoice.setChoiceId(rowSet1.getInt("choice_id"));
-//                regularChoice.setName(rowSet1.getString("name"));
-//                regularToppings.add(regularChoice);
-//            }
-//
-//            String sqlPremium =   "SELECT s.choice_id, c.name " +
-//                        "FROM choices_specialty_pizza AS s " +
-//                        "JOIN choices AS c ON s.choice_id = c.choice_id " +
-//                        "WHERE s.specialty_id = ? AND c.category_id = 5";
-//            SqlRowSet rowSet2 = jdbcTemplate.queryForRowSet(sqlPremium, specialtyPizza.getSpecialtyId());
-//            while (rowSet2.next()) {
-//                Choice premiumChoice = new Choice();
-//                premiumChoice.setChoiceId(rowSet2.getInt("choice_id"));
-//                premiumChoice.setName(rowSet2.getString("name"));
-//                premiumToppings.add(premiumChoice);
-//            }
-//
-//            specialtyPizza.setRegularToppings(regularToppings);
-//            specialtyPizza.setPremiumToppings(premiumToppings);
-//
-//
-//            result.add(specialtyPizza);
-//            }
-//        return result;
-//    }
 
     @Override
     public void addSpecial(SpecialtyPizza specialtyPizza) {
@@ -125,6 +102,5 @@ public class JdbcSpecialtyPizzaDAO implements SpecialtyPizzaDAO {
             addChoiceSpecial(choice, specialtyId);
         }
     }
-
 
 }
