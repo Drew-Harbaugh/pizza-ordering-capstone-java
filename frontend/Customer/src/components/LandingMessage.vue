27be9ebaad94532dbf1ac0a5c81d7c26285e9f8c
Jordan Jackson <jacks3jk@outlook.com>
2020-12-16 10:38:18 -0500

    finished landing page, fixed bugs, finished order functionality

---
+++ frontend/Customer/src/components/LandingMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <p>Thanks for ordering, {{$store.state.customerName}}</p>
+      <p>Your order will be ready soon!</p>
+  </div>
+</template>
+
+<script>
+export default {
+    destroyed() {
+        this.$store.commit("RESET_DATA_STORE");
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/Customer/src/views/Landing.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header-bar />
+    <landing-message />
+  </div>
+</template>
+
+<script>
+import LandingMessage from "@/components/LandingMessage.vue";
+import HeaderBar from "@/components/HeaderBar.vue";
+
+export default {
+  components: {
+    HeaderBar,
+    LandingMessage,
+  },
+};
+</script>
+
+<style>
+</style>
